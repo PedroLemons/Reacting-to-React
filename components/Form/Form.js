f88f2225f84706f1e2e0aef75e2127ebf356544c
PedroLemons <p.a.lemos1@gmail.com>
2023-01-31 23:55:39 -0300

    updating project

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,15 +1,36 @@
+import Button from '../Button'
+import Dropdown from '../Dropdown'
 import TextBox from '../TextBox/TextBox'
 import './Form.css'
 
 export const Form = () => {
 
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form enviado')
+    }
+
     return (
         <section className='form'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card de colaborador</h2>
                 <TextBox label="Nome" placeholder="Digite seu Nome" />
                 <TextBox label="Cargo" placeholder="Digite seu Cargo" />
                 <TextBox label="Imagem" placeholder="Digite o endereço da Imagem" />
+                <Dropdown label ="Time" itens={times} />
+                <Button>
+                    Criar card
+                </Button>
             </form>
         </section>
     )
